262cbdea5511ef1e89297bef2200d4b5bc7d9c4e
khalille97 <113073317+khalille97@users.noreply.github.com>
2022-09-29 10:53:42 +0100

    Added a Fouls and Periods button and display

--- index.js
+++ index.js
@@ -1,41 +1,67 @@
 let countEl = document.getElementById("home-count")
 let countAl = document.getElementById("guest-count")
+let periodCount = document.getElementById("period-count")
+let foulCountH = document.getElementById("fouls-counth")
+let foulCountA = document.getElementById("fouls-counta")
+
+
 console.log(countEl)
 console.log(countAl)
+console.log(periodCount)
+console.log(foulCountH)
+console.log(foulCountA)
 
 
-let countHome = 0
-let countAway = 0
 
+let count = 0
+let countg = 0
+let periodcount = 1
+let foulHome = 0
+let foulAway = 0
 
 function increment1(){
-    countHome += 1
-    countEl.textContent=countHome
-    console.log(countHome)
+    count += 1
+    countEl.textContent=count
+    console.log(count)
 }
 function increment2(){
-    countHome += 2
-    countEl.textContent=countHome
-    console.log(countHome)
+    count += 2
+    countEl.textContent=count
+    console.log(count)
 }
 function increment3(){
-    countHome += 3
-    countEl.textContent=countHome
-    console.log(countHome)
+    count += 3
+    countEl.textContent=count
+    console.log(count)
 }
 function increment01(){
-    countAway += 1
-    countAl.textContent=countAway
-    console.log(countAway)
+    countg += 1
+    countAl.textContent=countg
+    console.log(countg)
 }
 function increment02(){
-    countAway += 2
-    countAl.textContent=countAway
-    console.log(countAway)
+    countg += 2
+    countAl.textContent=countg
+    console.log(countg)
 }
 function increment03(){
-    countAway += 3
-    countAl.textContent=countAway
-    console.log(countAway)
+    countg += 3
+    countAl.textContent=countg
+    console.log(countg)
+}
+function incrementperiod(){
+    periodcount+=1
+    periodCount.textContent=periodcount
+    console.log(periodcount)
+}
+function incrementFoulsh(){
+    foulHome += 1
+    foulCountH.textContent = foulHome
+    console.log(foulHome)
+}
+function incrementFouls(){
+    foulAway += 1
+    foulCountA.textContent = foulAway
+    console.log(foulAway)
 }
 
